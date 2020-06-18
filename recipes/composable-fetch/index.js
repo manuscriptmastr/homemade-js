c62bed37a6186558d07f449f9c91a8823a48c7e5
Joshua Martin <the.manuscriptmaster@gmail.com>
2020-06-18 16:12:05 -0400

    Simplify timeout and headers recipes

--- recipes/composable-fetch/index.js
+++ recipes/composable-fetch/index.js
@@ -21,19 +21,19 @@ export const bearerAuthHeader = (token) =>
 
 // fetch decorators
 export const options = curry((decorate, fetch) => pipe(async (url, opts = {}) => [url, mergeDeepLeft(opts, is(Function, decorate) ? await decorate(opts) : decorate)], andThen(apply(fetch))));
-export const option = curry((key, value, fetch) => options({ [key]: value }, fetch));
-export const headers = curry((hdrs, fn) => options(async () => ({ headers: is(Function, hdrs) ? await hdrs() : hdrs }), fn));
+export const option = curry((key, value, fetch) => options(async () => ({ [key]: is(Function, value) ? await value() : value }), fetch));
 export const method = option('method');
+export const headers = option('headers');
+export const timeout = curry((ms, fetch) => option('signal', () => {
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), ms);
+  return controller.signal;
+})(fetch));
 export const body = fetch => (json, url, opts = {}) => options({
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify(json)
 }, fetch)(url, opts);
-export const timeout = curry((ms, fetch) => options(() => {
-  const controller = new AbortController();
-  setTimeout(() => controller.abort(), ms);
-  return { signal: controller.signal };
-})(fetch));
 
 // applies multiple decorators to fetch
 export const decorate = (fn, decorators) => compose(...decorators)(fn);
